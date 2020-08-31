3acc2c1b123273122ff2be156d7a30b13a634e7b
aarturas <artalameister@gmail.com>
2020-08-31 23:32:19 +0200

    pat 3-444

--- wp-content/themes/RED2/ourContact/aboutUs.php
+++ wp-content/themes/RED2/ourContact/aboutUs.php
@@ -3,44 +3,30 @@
 
 <?php    wp_head();   ?>
 
+<div style="background-color:blue; padding-top:10px;" font-style="color:white;">
 
+   <h1 style="color:azure; padding-left:2em;">
+      About Us
+   </h1>
 
-   <div style="background-color:blue; padding-top:10px;" font-style="color:white;">
+   <p style="color:azure; padding:20px;">
+      We manufacture and sell the highest-quality<br>
+      textile products for babies and little children.<br>
+      We sell products made of various-fibre certified <br>
+      fabrics characteristic of high quality, delicate <br>
+      range of colours, and attractive design.<br>
+      We are engaged in wholesale and retail trade: <br>
+      we cooperate with Australian and foreign companies, <br>
+      we communicate directly with our consumers. <br>
+   </p>
 
+   <!-- -------- Nurodome "kelią" tiesiai iš interneto ------ -->
 
-      <h1 style="color:azure; padding-left:2em;">
-         About Us
-      </h1>
+   <a href="<?= get_permalink(get_page_by_path('p-c',OBJECT,'page')) ?>">
+      <img class="img" src="https://cdn.pixabay.com/photo/2015/05/06/16/31/andromeda-galaxy-755442_960_720.jpg"
+         alt="galaxy" style="padding-top:50px; max-width:100%; padding-bottom:0px;">
+   </a>
 
-
-      
-
-      <p style="color:azure; padding:20px;">
-         We manufacture and sell the highest-quality<br>
-         textile products for babies and little children.<br>
-         We sell products made of various-fibre certified <br>
-         fabrics characteristic of high quality, delicate <br>
-         range of colours, and attractive design.<br>
-         We are engaged in wholesale and retail trade: <br>
-         we cooperate with Australian and foreign companies, <br>
-         we communicate directly with our consumers. <br>
-      </p>
-      
-
-      
-      <!-- -------- Nurodome "kelią" tiesiai iš interneto ------ -->
-         
-      <a href="<?= get_permalink(get_page_by_path('p-c',OBJECT,'page')) ?>">
-         <img class="img" 
-            src="https://cdn.pixabay.com/photo/2015/05/06/16/31/andromeda-galaxy-755442_960_720.jpg"
-            alt="galaxy"
-            style="padding-top:50px; max-width:100%; padding-bottom:0px;">
-      </a>
-
-
-
-
-   </div>
-
+</div>
 
 <?php wp_footer(); ?>
--- wp-content/themes/RED2/single-our_contact.php
+++ wp-content/themes/RED2/single-our_contact.php
@@ -1,51 +1,50 @@
 <?php     get_header();   ?>
-   
-   
-   <div style="background-color:blue; padding-top:10px;" font-style="color:white;">
 
+<div style="background-color:blue; padding-top:10px;" font-style="color:white;">
 
+   <!-- --------------------------------------------------------------------------------------------- -->
+   <!-- ---------- Puslapis reikalingas supaprastinti funkcijas WP skaitymui ir redagavimui --------- -->
+   <!-- ---------- Single "įdedame" nes, aprašome kiekvieną funkciją atskirai ir nereikia laikyti  -- -->
+   <!-- ---------- VS Code meniu langelyje kaireje, viskas pereina i "sinle-our_team" --------------- -->
+   <!-- ---------- PVZ : title, puslapyje bus atvaizduojamas 'žmogaus' vardas ar pavardė ------------ -->
+   <!-- ------- Po to WP atsiranda langeliai, kuriuose galima bus įrašyti/užpildyti vardą ar aprašymą -->
+   <!-- --------------------------------------------------------------------------------------------- -->
 
-<!-- ---------- Puslapis reikalingas supaprastinti funkcijas WP skaitymui ir redagavimui --------- -->
-<!-- ---------- Single "įdedame" nes, aprašome kiekvieną funkciją atskirai ir nereikia laikyti  ----------------- -->
-<!-- ---------- VS Code meniu langelyje kaireje, viskas pereina i "sinle-our_team" -->
-<!-- ---------- PVZ : title, puslapyje bus atvaizduojamas 'žmogaus' vardas ar pavardė ------------ -->
-<!-- ---------- Po to WP atsiranda langeliai, kuriuose galima bus įrašyti/užpildyti vardą ar aprašymą -->
+   <!-- --------------------------------------------------------------------------------------------- -->
+   <!--                                        TITLE                                                  -->
+   <!--                              Pavadinimo patalpinimas, <h1>                                    -->
+   <!-- --------------------------------------------------------------------------------------------- -->
 
+   <h1 style="color:azure; padding-left:4em; padding-top:15px;">
+      <?= get_field('menu-item') ?>
+   </h1>
 
-      <!-- --------------------------------- TITLE -------------------------------------- -->
-        <!-- ----------------------- Pavadinimo patalpinimas, <h1> --------------------------- -->
-      <h1 style="color:azure; padding-left:4em; padding-top:15px;">
-         <?= get_field('menu-item') ?>
-      </h1>
+   <!-- --------------------------------------------------------------------------------------------- -->
+   <!--                                DESCRIPTION - DESC                                             -->
+   <!--                            Paaiškinimo patalpinimas į WP                                      -->
+   <!-- --------------------------------------------------------------------------------------------- -->
 
-      <!-- --------------------------- DESCRIPTION - DESC ----------------------------------- -->
-        <!-- ----------------------- Paaiškinimo patalpinimas į WP --------------------------- -->
+   <div style="color:azure; padding:20px;">
+      <?= get_field('desc') ?>
+   </div>
 
-      <div style="color:azure; padding:20px;">
-         <?= get_field('desc') ?>
-      </div>
+   <!-- --------------------------------------------------------------------------------------------- -->
+   <!--                                       PHOTO                                                   -->
+   <!--                  Perkeliame "our contact" img tiesiai į WP, nes susikūrėme nuorodą            -->
+   <!--                  "single-our_contact", su nuoroda į 'photo'                                   -->
+   <!-- --------------------------------------------------------------------------------------------- -->
 
-     
-
-      <!--  ------------------------------ PHOTO ------------------------------------------------------------------ -->
-      <!-- Perkeliame "our contact" img tiesiai į WP, nes susikūrėme nuorodą "single-our_contact", su nuoroda į 'photo' -->
-      
-      <a href="<?= get_permalink(get_page_by_path('p-c',OBJECT,'page')) ?>">
+   <a href="<?= get_permalink(get_page_by_path('p-c',OBJECT,'page')) ?>">
 
       <img src="<?= get_field('photo') ?>" alt="<?= get_field('alt') ?>">
 
-</a>
+   </a>
 
-           
- <!-- ----------------- Grįžimas į pagrindinį langą -------------------------------------- -->
-      <!--  --------------"permalink" funkcija "duoda" matyti puslapį ne tik iš 'localhost'o' ------------ -->
+   <!-- --------------------------------------------------------------------------------------------- -->
+   <!--                     Grįžimas į pagrindinį langą                                               -->
+   <!--           "permalink" funkcija "duoda" matyti puslapį ne tik iš 'localhost'o'                 -->
+   <!-- --------------------------------------------------------------------------------------------- -->
 
-     
+</div>
 
-
-
-
-    </div>
-
-
-    <?php     get_footer();   ?>
+<?php     get_footer();   ?>
